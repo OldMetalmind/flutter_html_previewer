96a4291fca016707db488ff8fe39eeb044baa024
Filipe Barroso <filipe.barroso@gmail.com>
2020-03-29 22:33:17 +0100

    Add firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'HTML Flutter Preview',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
